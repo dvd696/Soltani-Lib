49cdec9a886b3fcb3763967472d8c9f8297e9323
davoud nazari <davoudnzr@gmail.com>
2024-02-07 23:23:24 +0330

    14th commit:Complete About Us part

--- ShahidSoltaniLibrary.App/AboutUsForm.cs
+++ ShahidSoltaniLibrary.App/AboutUsForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -30,6 +31,31 @@ namespace ShahidSoltaniLibrary.App
         private void label8_Click(object sender, EventArgs e)
         {
             DialogResult = DialogResult.OK;
+        }
+
+        private void btnInsta_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://instagram.com/dvd.696");
+        }
+
+        private void btnTel_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://t.me/dvd696");
+        }
+
+        private void btnLinked_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.linkedin.com/in/davoud-nazari-41813923b");
+        }
+
+        private void btnFace_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://facebook.com/dvd.696");
+        }
+
+        private void btnQuera_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://quera.org/profile/hftvqg");
         }
     }
 }
